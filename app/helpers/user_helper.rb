15e754dd57dc4383bca0f33a478ec2b8633306b0
Julian Nadeau <julian.nadeau@jadedpixel.com>
2014-06-12 00:08:25 -0400

    Divide by zero avoidance!

--- app/helpers/user_helper.rb
+++ app/helpers/user_helper.rb
@@ -1,6 +1,7 @@
 module UserHelper
 
   def average_of_groups(groups)
+    return 'N/A' if groups.count == 0
     (((groups.inject(0){|sum, group| sum + group.percent_correct })/groups.count)).round(1)
   end
 end
