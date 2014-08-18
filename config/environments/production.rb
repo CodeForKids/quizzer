dd866d25986d019bdafbc632839c22678a8ad79d
Julian Nadeau <julian.nadeau@jadedpixel.com>
2014-08-18 18:53:41 -0400

    Update paper trail

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -49,7 +49,7 @@ Rails.application.configure do
   # config.log_tags = [ :subdomain, :uuid ]
 
   # Use a different logger for distributed setups.
-  config.logger = RemoteSyslogLogger.new('logs2.papertrailapp.com', 42000)
+  config.logger = RemoteSyslogLogger.new('logs2.papertrailapp.com', 15521)
 
   # Use a different cache store in production.
   # config.cache_store = :mem_cache_store
